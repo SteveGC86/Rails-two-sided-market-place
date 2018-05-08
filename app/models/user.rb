bac88409ad46f4fc778aad3442c9c8147f2c170d
Steven Christenson <stevenchristenson@Stevens-MacBook-Air.local>
2018-05-08 17:19:24 +1000

    Profile model addded

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  # protect_from_forgery prepend:true
+  # before_action :authenticate_user!
+  has_one :profile
+end
