15412b35c7e52a0c8aacde923a5ddcb7ae03a3c0
Steven Christenson <stevenchristenson@Stevens-MacBook-Air.local>
2018-05-10 09:01:48 +1000

    Add cosmetic changes to profile/show page

--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-<<<<<<< HEAD
-  root to: "home#index"
-  
-=======
   root to: "shopping_lists#index"
 
   get '/home', to: "pages#home"
 
->>>>>>> development
 end
